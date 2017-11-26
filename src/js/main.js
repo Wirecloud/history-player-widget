2d26f49092a51aee5ac24ed23f18051e3688485a
Álvaro Arranz <aarranz@ficodes.com>
2017-11-26 01:52:22 +0100

    Add a play button

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
-/* globals $, d3, moment, nv */
+/* globals $, d3, moment, nv, StyledElements */
 
-(function () {
+(function (se) {
 
     "use strict";
 
@@ -9,6 +9,10 @@
     var initialData = [];
     var initialMin = null;
     var initialMax = null;
+    var currentMoment = null;
+    var currentStep = null;
+    var interval = null;
+    var playbutton = null;
 
     var resizeLines = function removeLines() {
         d3.select(chart.container).selectAll(".history-current-value line")
@@ -39,6 +43,16 @@
         var svg = element.children("svg").get(0);
         d3.select("#chart").classed("svg-content-responsive", true);
 
+        playbutton = new se.ToggleButton({iconClass: "fa fa-play"});
+        playbutton.addEventListener('click', () => {
+            if (playbutton.active) {
+                play();
+            } else {
+                stop();
+            }
+        });
+        playbutton.appendTo(document.body);
+
         nv.addGraph(function () {
 
             // By the moment taking default params
@@ -46,24 +60,32 @@
                 .useInteractiveGuideline(true)
                 .showYAxis(false)
                 .interpolate("monotone")
-                .duration(250);
+                .margin({top: 30, right: 10, bottom: 25, left: 40})
+                .duration(125);
 
             chart.xAxis.tickFormat((d) => {
                 return moment(d).format("L");
             });
             chart.xScale(d3.time.scale());
 
+            chart.forceY(0);
+
             // Tooltip
             chart.interactiveLayer.tooltip.contentGenerator((d) => {
                 return "<h2>" + moment(d.value).format("llll") + "</h2>";
             });
 
+            if (initialMin != null && initialMax != null) {
+                chart.forceX([initialMin, initialMax]);
+            }
             chartData = d3.select(svg).datum(initialData);
-            chartData.transition().duration(500).call(chart);
+            chartData.transition().duration(125).call(chart);
 
             // Event listeners
             chart.lines.dispatch.on("elementClick", function (e) {
                 removeLines();
+                currentMoment = e[0].point.x;
+                currentStep = e[0].pointIndex;
                 renderLine(e[0].point.x);
                 MashupPlatform.wiring.pushEvent("outputData", e[0].point.data);
             });
@@ -75,10 +97,56 @@
         }.bind(this));
     };
 
+    var nextStep = function nextStep() {
+        var data = chartData.datum()[0].values;
+
+        currentStep += 1;
+        if (currentStep < data.length) {
+            currentMoment = data[currentStep].x;
+            removeLines();
+            renderLine(currentMoment);
+            MashupPlatform.wiring.pushEvent("outputData", data[currentStep].data);
+        } else {
+            stop();
+        }
+    };
+
+    var play = function play() {
+        var data = chartData.datum()[0].values;
+        if (currentStep >= data.length) {
+            currentStep = 0;
+        } else if (data.length == 0) {
+            playbutton.active = false;
+            return;
+        }
+        interval = setInterval(nextStep, 1000);
+        nextStep();
+    };
+
+    var stop = function stop() {
+        if (interval != null) {
+            clearInterval(interval);
+            interval = null;
+        }
+        playbutton.active = false;
+    };
+
     var paint = function paint(data, min, max) {
+
         if (chartData != null) {
             chart.forceX([min, max]);
-            chartData.datum(data).transition().duration(500).call(chart);
+            chartData.datum(data).transition().duration(125).call(chart);
+            if (currentMoment == null) {
+                currentStep = nv.interactiveBisect(data[0].values, max, chart.x());
+                currentMoment = data[0].values[currentStep].x;
+                renderLine(currentMoment);
+                MashupPlatform.wiring.pushEvent("outputData", data[0].values[currentStep].data);
+            } else {
+                currentStep = nv.interactiveBisect(data[0].values, currentMoment, chart.x());
+                currentMoment = data[0].values[currentStep].x;
+                removeLines();
+                renderLine(currentMoment);
+            }
         } else {
             initialData = data;
             initialMin = min;
@@ -122,4 +190,4 @@
 
     config();
 
-})();
+})(StyledElements);
